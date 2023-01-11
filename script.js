a06d399d75ea4d7b7de9592e8eef4745c0c9ea5f
Priyaas2326 <113659062+Priyaas2326@users.noreply.github.com>
2023-01-11 20:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 // complete the given function
 
 function palindrome(str){
- // get the total length of the words  
-    const len = string.length;  
-  
-    // Use for loop to divide the words into 2 half  
-    for (let i = 0; i < len / 2; i++) {  
-  
-        // validate the first and last characters are same  
-        if (string[i] !== string[len - 1 - i]) {  
-            alert( 'It is not a palindrome');  
-        }  
-    }  
-    alert( 'It is a palindrome');  
-}  
-  
-// accept the string or number from the prompt  
-const string = prompt('Enter a string or number: ');  
-  
-const value = validatePalin(string);  
-  
-console.log(value);  
+ const n = str.length;
+	for(let i=0; i<n/2; i++){
+		if(str[i].toLowerCase() !== (str[n-1-i].toLowerCase())){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
